8c4cb290e57887bff2560a0f9345364ae060852f
Ronaldy <ronaldylf@gmail.com>
2024-06-24 20:47:06 -0300

    changed enviroment variable names and added 'DB_PORT'

--- shared/database.py
+++ shared/database.py
@@ -8,9 +8,10 @@ load_dotenv('.env') # load .env variables
 
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_ADDRESS = os.getenv('DB_ADDRESS')
+DB_HOST = os.getenv('DB_HOST')
 DB = os.getenv('DB')
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}/{DB}"
+DB_PORT = os.getenv('DB_PORT')
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
